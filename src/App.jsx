2122b11fd8fcaae26e69b6b468d1edbec4206e6c
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-24 18:34:41 -0500

    added a head with nav links and styled the links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <h1>I said!!! ... please?</h1>
+      <Header />
+      <section>
+        <h1>I said!!! ... please?</h1>
+      </section>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <button>Home</button>
+      <button>Menu</button>
+      <button>About</button>
+    </header>
+  )
+}
+
+export default Header
