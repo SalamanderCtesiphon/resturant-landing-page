1138dff699cf91d2e3e7cde898b2ebcd95a2a429
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-24 21:02:05 -0500

    fixed an issue with the header moving around unexpectedly

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 function About() {
   return (
-    <div className="about">
-      We are Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-      omnis consequuntur, voluptate tempora, esse magnam doloremque ducimus
-      laboriosam quod numquam commodi distinctio harum totam a, delectus nulla
-      sequi. Ratione, natus.
-    </div>
+    <>
+      <section>
+        <p className="about">
+          We are Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+          omnis consequuntur, voluptate tempora, esse magnam doloremque ducimus
+          laboriosam quod numquam commodi distinctio harum totam a, delectus
+          nulla sequi. Ratione, natus.
+        </p>
+      </section>
+    </>
   )
 }
 
