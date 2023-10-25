b2db7531f20022cbe383dad980009df1c1671ac5
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-24 20:50:00 -0500

    implemented a reservations form

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
 
 function Reservation() {
-  return <div>Reservation</div>
+  return (
+    <form className="form">
+      <p className="title">Book a Reservation</p>
+      <p className="message">
+        Please fill out the form below to book your reservation.
+      </p>
+      <div className="flex">
+        <label>
+          <input className="input" type="text" placeholder="" required />
+          <span>Firstname</span>
+        </label>
+        <label>
+          <input className="input" type="text" placeholder="" required />
+          <span>Lastname</span>
+        </label>
+      </div>
+      <label>
+        <input className="input" type="email" placeholder="" required />
+        <span>Email</span>
+      </label>
+      <label>
+        <input className="input" type="text" placeholder="" required />
+        <span>Date and Time</span>
+      </label>
+      <label>
+        <input className="input" type="text" placeholder="" required />
+        <span>Confirm your reservation time and date</span>
+      </label>
+      <button className="submit">Submit</button>
+    </form>
+  )
 }
 
 export default Reservation
