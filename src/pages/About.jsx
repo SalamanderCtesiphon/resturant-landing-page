719ad0dbc77e65782b3a1b9b2cc7a6d6f1b7cda0
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-24 21:40:33 -0500

    finalized some of the rough styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,12 +4,20 @@ function About() {
   return (
     <>
       <section>
+        <h2>We are Resturant Page!</h2>
+        <h4>welcome to resturant page... </h4>
         <p className="about">
           We are Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
           omnis consequuntur, voluptate tempora, esse magnam doloremque ducimus
           laboriosam quod numquam commodi distinctio harum totam a, delectus
           nulla sequi. Ratione, natus.
         </p>
+        <span>
+          Email us:{' '}
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            resturantPage@resturantPage.com
+          </a>
+        </span>
       </section>
     </>
   )
